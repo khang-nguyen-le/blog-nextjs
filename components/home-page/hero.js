2892018d06dde888601c9863170612ff6b798d36
khang-nguyen-le <khang.ln.210@gmail.com>
2024-02-25 16:28:09 +0700

    .

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -13,7 +13,7 @@ function Hero() {
           height={300}
         />
       </div>
-      <h1>Hi. I&apos;m Khang</h1>
+      <h1>Hi. I&apos;m Khangg</h1>
       <p>
         I blog about web development - especially frontend frameworks like
         NextJS or Angular
